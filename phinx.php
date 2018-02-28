304011fa42edb4597e1252874569e253854c8493
oscarotero <oom@oscarotero.com>
2018-02-28 19:25:46 +0100

    usar configuracion en php

---
+++ phinx.php
@@ -0,0 +1,26 @@
+<?php
+
+include __DIR__.'/vendor/autoload.php';
+
+//Init .env variables
+(new Dotenv\Dotenv(__DIR__))->load();
+
+Env::init();
+
+//Execute the app
+$app = new Jose\App();
+
+return [
+    'paths' => [
+        'migrations' => __DIR__.'/db/migrations',
+        'seeds' => __DIR__.'/db/seeds',
+    ],
+
+    'environments' => [
+        'default_database' => 'production',
+        'production' => [
+            'name' => 'jose',
+            'connection' => $app->get('pdo'),
+        ],
+    ],
+];
